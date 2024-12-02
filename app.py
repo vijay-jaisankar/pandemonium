3deb3a7b12d4515d1df482ed4f128a778f128a61
mimeosd@yahoo.com <mimeosd@yahoo.com>
2024-12-02 17:28:56 +0100

    refactor: removed changelog comments from app.py as per review

--- app.py
+++ app.py
@@ -26,18 +26,3 @@ def sitemap_xml():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# What`s done?
-# 1. Removed global statements since its bad practice to rely on constant that some other function in future might modify
-# 2. get_meme_url Result Check - used or to handle None responses more concisely.
-# 3. Simplified the Logic: Removed unnecessary checks or redundant variable assignments.
-# 4. Added more meta tags as per open issue on GH page
-# 5. Removed styles.css - you dont need it since we are inheriting from bootstrap`s css. Unless you specifically want to style page yourself, you do not need styles.css
-# 6. Added robots.txt and sitemap.xml. Since you have static site there is no need for dynaminc generation of urls (you have only index.html)
-# Thus when you deploy this website to production make sure to modify both files, eg:
-#   robots.txt
-#  Sitemap: https://www.pandemonium-memes.com/sitemap.xml
-#   sitemap.xml
-#  <loc>https://www.pandemonium-memes.com/</loc>
-# Documentation - what do you want to document? Only 1 webpage with 1 function and 1 API call so I think documenting it is redundant...
-# 7. In index.html, in head section modify author and twitter handle to your, I put mine for now.
